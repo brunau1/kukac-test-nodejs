7d37935cd6a428b07922a4645aee8e6a3a97b387
brunau1 <brunosantosprotec@gmail.com>
2019-08-07 14:54:23 -0300

    implemented methods get and post on the controller

--- server/source/controllers/Moto.controller.ts
+++ server/source/controllers/Moto.controller.ts
@@ -1,20 +1,25 @@
 import { Request, Response } from 'express';
-import { saveObject } from '../utils/file'
+import { saveObject, readFile } from '../utils/file'
 import Moto from '../models/Moto'
 
 export default class MotoController {
     public static get(request: Request, response: Response) {
-        return response.status(200).send();
+        (async () => {
+            const data: any = await readFile('../filesJson/motoObjects.json')
+            if (!!data.error)
+                return response.status(500).send(data);
+            else return response.status(200).send(data);
+        })();
     }
 
     public static post(request: Request, response: Response) {
         const { atributes } = request.body;
-        const motoObject = new Moto(atributes);
+        const motoObject: Moto = new Moto(atributes);
         if (!!motoObject) {
             (async () => {
                 const message = await saveObject(atributes, '../filesJson/motoObjects.json');
-                if (message.error)
-                    return response.status(500).send(message);
+                if (!!message.error) return response.status(500).send(message);
+                else return response.status(200).send(message);
             })();
         }
         else return response.status(400).send({
