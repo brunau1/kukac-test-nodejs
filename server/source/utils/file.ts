64557389e1d17dd821eca2ba575edcf2b68110fb
brunau1 <brunosantosprotec@gmail.com>
2019-08-07 14:53:15 -0300

    created all the functions in the file utilities

---
+++ server/source/utils/file.ts
@@ -0,0 +1,64 @@
+import fs from 'fs'
+export function toJson(data: {}) {
+    return JSON.stringify(data, null, 2);
+}
+export async function readFile(archiveName: string) {
+    try {
+        const data = await fs.readFileSync(archiveName);
+        return data
+    } catch (error) {
+        return {
+            'error': true,
+            'message': 'failed to read file',
+            'thrownError': error
+        };
+    }
+}
+export async function writeFile(archiveName: string, data: string | JSON) {
+    try {
+        await fs.writeFileSync(archiveName, data);
+    } catch (error) { return error }
+}
+export async function openFile(archiveName: string, flag: string) {
+    try {
+        await fs.openSync(archiveName, flag);
+    } catch (error) { return error }
+}
+export async function appendFile(archiveName: string, data: string | JSON) {
+    try {
+        await fs.appendFileSync(archiveName, data);
+    } catch (error) { return error }
+}
+export async function saveObject(object: {}, archiveName: string) {
+    const objectJson = toJson(object);
+    openFile(archiveName, 'w').then().catch(error => {
+        return {
+            'error': true,
+            'message': 'failed to open file',
+            'thrownError': error
+        };
+    })
+    readFile(archiveName).then((data) => {
+        if (!data)
+            writeFile(archiveName, objectJson).then().catch(error => {
+                return {
+                    'error': true,
+                    'message': 'failed to write file',
+                    'thrownError': error
+                };
+            })
+        else
+            appendFile(archiveName, objectJson).then().catch(error => {
+                return {
+                    'error': true,
+                    'message': 'failed to append file',
+                    'thrownError': error
+                };
+            })
+    }).catch()
+
+    return {
+        'error': false,
+        'message': 'object created and saved'
+    };
+}
