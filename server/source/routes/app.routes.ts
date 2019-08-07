9287157e3c848ec4efb65d03a6f8aca411cc6052
brunau1 <brunosantosprotec@gmail.com>
2019-08-07 14:53:37 -0300

    added the moto class routes

--- server/source/routes/app.routes.ts
+++ server/source/routes/app.routes.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from "express";
 import PalindromeController from "../controllers/Palindrome.controller";
 import CashController from "../controllers/Cash.controller";
+import MotoController from "../controllers/Moto.controller";
 
 function defaultRoute(request: Request, response: Response){
     response.send({
@@ -19,6 +20,16 @@ routes.get('/palindrome', PalindromeController.get);
  * Retorna uma um objeto com a quantidade de cada nota para o troco
  */
 routes.get('/cash', CashController.get);
+
+/* 
+* Retorna um objeto JSON com todos os objetos da classe moto
+*/
+routes.get('/moto', MotoController.get)
+
+/* 
+* Cria um objeto da classe moto e salva em um arquivo JSON
+*/
+routes.post('/moto/create', MotoController.post)
 
 /*
 * Rota default da API - teste
