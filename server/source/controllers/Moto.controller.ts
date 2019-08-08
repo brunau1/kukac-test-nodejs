d8d09b5456f1ca2078500758b043af7f6726e977
brunau1 <brunosantosprotec@gmail.com>
2019-08-08 13:35:29 -0300

    finished the moto controller methods

--- server/source/controllers/Moto.controller.ts
+++ server/source/controllers/Moto.controller.ts
@@ -1,14 +1,19 @@
 import { Request, Response } from 'express';
-import { saveObject, readFile } from '../utils/file'
-import Moto from '../models/Moto'
+import { saveObject, readFile } from '../utils/file';
+import path from 'path';
+import Moto from '../models/Moto';
+const filePath = path.resolve(__dirname,'../../../','database','moto.json');
 
 export default class MotoController {
     public static get(request: Request, response: Response) {
         (async () => {
-            const data: any = await readFile('../filesJson/motoObjects.json')
+            const data: any = await readFile(filePath)
             if (!!data.error)
                 return response.status(500).send(data);
-            else return response.status(200).send(data);
+            else return response.status(200).send({
+                'error': false,
+                'data': JSON.parse(data.toString())
+            });
         })();
     }
 
@@ -17,7 +22,7 @@ export default class MotoController {
         const motoObject: Moto = new Moto(atributes);
         if (!!motoObject) {
             (async () => {
-                const message = await saveObject(atributes, '../filesJson/motoObjects.json');
+                const message = await saveObject(atributes, filePath);
                 if (!!message.error) return response.status(500).send(message);
                 else return response.status(200).send(message);
             })();
