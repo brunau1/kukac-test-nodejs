6d3cfe24f89f088aa197dacc55998246a5293acb
brunau1 <brunosantosprotec@gmail.com>
2019-08-09 16:09:42 -0300

    created all aplication routes

--- server/source/routes/app.routes.ts
+++ server/source/routes/app.routes.ts
@@ -2,6 +2,8 @@ import { Router, Request, Response } from "express";
 import PalindromeController from "../controllers/Palindrome.controller";
 import CashController from "../controllers/Cash.controller";
 import MotoController from "../controllers/Moto.controller";
+import TourController from "../controllers/Tour.controller";
+import CepController from "../controllers/Cep.controller";
 
 function defaultRoute(request: Request, response: Response){
     response.send({
@@ -30,6 +32,21 @@ routes.get('/moto', MotoController.get)
 * Cria um objeto da classe moto e salva em um arquivo JSON
 */
 routes.post('/moto/create', MotoController.post)
+
+/* 
+* Retorna um objeto JSON com todos os objetos da classe passeio (tour)
+*/
+routes.get('/passeio', TourController.get)
+
+/* 
+* Retorna um objeto JSON com todos os atributos do cep
+*/
+routes.get('/cep', CepController.get)
+
+/* 
+* Cria um objeto da classe passeio (tour) e salva em um arquivo JSON
+*/
+routes.post('/passeio/create', TourController.post)
 
 /*
 * Rota default da API - teste
