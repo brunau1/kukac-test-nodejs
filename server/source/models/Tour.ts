eede7c394e350e6e18542b708afa1780f145d1d9
brunau1 <brunosantosprotec@gmail.com>
2019-08-09 16:09:17 -0300

    implemented get and post methods to class tour

--- server/source/models/Tour.ts
+++ server/source/models/Tour.ts
@@ -1,27 +1,29 @@
-interface TourInterface{
-    model: string;
-    yearOfManofacture: string;
-    readonly doorQuantitie: number;
-    brand: string;
-    text?: string | {};
-}
+import VehicleInterface from "./Vehicle";
 
-export default class Tour implements TourInterface{
+export default class Tour implements VehicleInterface{
     model: string;    
     yearOfManofacture: string;
     doorQuantitie: number;
     brand: string;
     text?: string | {};
 
-    constructor(atributes: TourInterface){
+    constructor(atributes: VehicleInterface){
         this.model = atributes.model;
         this.yearOfManofacture = atributes.yearOfManofacture;
         this.doorQuantitie = 4;
         this.brand = atributes.brand;
         this.text = atributes.text;
 
-        if(!this.model || !this.yearOfManofacture || this.brand)
+        if(!this.model || !this.yearOfManofacture || !this.brand)
             throw 'your request is missing some parameters';
+    }
 
+    public toObject(){
+        return{
+            'model': this.model,
+            'yearOfManofacture': this.yearOfManofacture,
+            'doorQuantitie': this.doorQuantitie,
+            'brand': this.brand
+        }
     }
 }
