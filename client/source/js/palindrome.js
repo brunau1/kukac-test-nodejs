4ca96138ac3b41d536ec47ee21483b9dc4f458b8
brunau1 <brunosantosprotec@gmail.com>
2019-08-11 02:24:55 -0300

    the method to consult the local api not work

--- client/source/js/palindrome.js
+++ client/source/js/palindrome.js
@@ -2,15 +2,22 @@ const host = 'http://localhost:3003/api/v1'
 
 var getPalindromeNumbers = async () => {
     const firstNumber = document.querySelector('#inputFirstNumber').value || 0
-    const lastNumber = document.querySelector('#inputLastNumber').value|| 0
+    const lastNumber = document.querySelector('#inputLastNumber').value || 0
 
     var asyncGetPalindromes = () => {
         try {
             return new Promise((resolve, reject) => {
                 $.ajax({
-                    url: `${host}/moto`,
+                    url: `${host}/palindrome?first=${firstNumber}&last=${lastNumber}`,
                     type: 'get',
-                    dataType: 'jsonp'
+                    dataType: 'json',
+                    crossDomain: true,
+                    contentType: "application/json",
+                    headers: {
+                        "Accept": 'application/json',
+                        "Access-Control-Allow-Origin": 'http://localhost:3003',
+                        "Access-Control-Allow-Credentials": 'true'
+                    }
                 })
                     .done(data => {
                         resolve(data)
@@ -22,13 +29,13 @@ var getPalindromeNumbers = async () => {
         } catch (error) { return error }
     }
     const palindromes = await asyncGetPalindromes()
-    showPalindromeNumbers(palindromes)
+    return palindromes
 }
 
-var showPalindromeNumbers = (palindromes) => {
+var showPalindromeNumbers = async () => {
+    const palindromes = await getPalindromeNumbers()
     const paragraph = document.querySelector('#show-palindromes')
     paragraph.innerHTML = palindromes
 }
 
-
-document.querySelector('#button-generate').addEventListener('click', getPalindromeNumbers)
+document.querySelector('#button-generate').addEventListener('click', showPalindromeNumbers)
